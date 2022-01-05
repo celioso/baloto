9f504156c99f8be7446bf9bf56674b16b66a1c59
celioso <41644949+celioso@users.noreply.github.com>
2022-01-04 19:27:39 -0500

    Add files via upload

---
+++ powerball.js
@@ -0,0 +1,32 @@
+var z;
+
+for(var i = 0; i < 5; i++ )
+{
+  z = aleatorio(1, 69);
+  document.write(" <br />  " + z +  " <br />  " );
+}
+
+function aleatorio(min, max)
+{
+  var resultado;
+  resultado = Math.floor(Math.random() * (max - min + 1)) + min;
+  return resultado;
+}
+
+//numeros para el baloto
+var a;
+
+for(var i = 0; i < 1; i++ )
+{
+
+  a = aleatorio1(1, 26);
+  //document.write(";" + a);
+  document.write( "<br/> super balota " + a + "<br/> ");
+}
+
+function aleatorio1(min, max)
+{
+  var resultado1;
+  resultado1 = Math.floor(Math.random() * (max - min + 1)) + min;
+  return resultado1;
+}
